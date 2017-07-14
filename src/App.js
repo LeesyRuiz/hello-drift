eb0d3c2a2edbbb520bad409f4ee6b286593bc1a6
leesyruiz <leesyruiz@gmail.com>
2017-07-14 11:09:00 -0700

    date & time picker working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
+import enUS from 'antd/lib/locale-provider/en_US';
+import { DatePicker } from 'antd';
+import { TimePicker } from 'antd';
+
+const format = 'YYYY-MM-DD HH:mm';
+
+function disabledSeconds() {
+}
+
+function onChange(value, dateString) {
+  console.log('Selected Time: ', value);
+  console.log('Formatted Selected Time: ', dateString);
+}
+
+function onOk(value) {
+  console.log('onOk: ', value);
+}
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+      <DatePicker
+      showTime={{ format: 'HH:mm' }}
+      format={format}
+      placeholder="Select Date & Time"
+      onChange={onChange}
+      onOk={onOk}
+      disabledSeconds={disabledSeconds}
+      />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import { LocaleProvider } from 'antd';
+import enUS from 'antd/lib/locale-provider/en_US';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+
+
+ReactDOM.render(
+  <LocaleProvider locale={enUS}>
+    <App />
+  </LocaleProvider> ,document.getElementById('root')
+);
